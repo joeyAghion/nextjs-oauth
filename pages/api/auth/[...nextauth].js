353cc56e6cfa4087378c6966fd979dc69eb8320a
Joey Aghion <joey@aghion.com>
2022-03-31 16:31:41 -0400

    Fix userinfo request, work around token request for now

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,54 +1,41 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-import { authenticatedPost } from "openid-client/lib/helpers/client"
 
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
-        {
-          id: "artsy",
-          clientId: process.env.GRAVITY_CLIENT_ID,
-          clientSecret: process.env.GRAVITY_CLIENT_SECRET,
-          name: "Artsy",
-          type: "oauth",
-          authorization: "https://stagingapi.artsy.net/oauth2/authorize",
-          token: {
-            url: "https://stagingapi.artsy.net/oauth2/access_token",
-            async request(context) {
-              console.log("context", context) //DEBUG
-              // context contains useful properties to help you make the request.
-              // const tokens = await makeTokenRequest(context)
-
-              const response = await authenticatedPost.call(
-                context.client,
-                'token',
-                {
-                  form: body,
-                  responseType: 'json',
-                },
-                { clientAssertionPayload, DPoP },
-              );
-              let responseBody;
-
-              return {} //{ tokens }
-            }
-          },
-          client: {
-            token_endpoint_auth_method: "client_secret_post",
-          },
-          userinfo: "https://stagingapi.artsy.net/api/v1/me",
-          profile(profile) {
-            return {
-              id: profile.id,
-              name: profile.name,
-              email: profile.email
-            }
-          },
-        }
+      GithubProvider({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      }),
+      {
+        id: "artsy",
+        clientId: process.env.GRAVITY_CLIENT_ID,
+        clientSecret: process.env.GRAVITY_CLIENT_SECRET,
+        name: "Artsy",
+        type: "oauth",
+        authorization: process.env.GRAVITY_AUTHORIZE_URL,
+        token: process.env.GRAVITY_TOKEN_URL,
+        client: {
+          token_endpoint_auth_method: "client_secret_post",
+        },
+        userinfo: {
+          url: process.env.GRAVITY_USERINFO_URL,
+          async request (context) {
+            const response = await fetch(context.provider.userinfo.url, {
+              headers: { "X-Access-Token": context.tokens.access_token } // override default of Authorization: Bearer ... token
+            })
+            return await response.json()
+          }
+        },
+        profile(profile) {
+          return {
+            id: profile?.id,
+            name: profile?.name,
+            email: profile?.email
+          }
+        },
+      }
     // ...add more providers here
     ],
 })
